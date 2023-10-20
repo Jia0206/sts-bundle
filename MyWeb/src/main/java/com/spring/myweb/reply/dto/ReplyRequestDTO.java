dc0a4eae1168734f3a8f4ee1e2714037f7f27a3e
jia <imjiaim@naver.com>
2023-10-20 17:36:06 +0900

    231020

---
+++ MyWeb/src/main/java/com/spring/myweb/reply/dto/ReplyRequestDTO.java
@@ -0,0 +1,34 @@
+package com.spring.myweb.reply.dto;
+
+import com.spring.myweb.reply.entity.Reply;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReplyRequestDTO {
+	
+	private int bno;
+	private String replyText;
+	private String replyId;
+	private String replyPw;
+
+	public Reply toEntity(ReplyRequestDTO dto) {
+		return Reply.builder()
+				.bno(dto.getBno())
+				.replyText(dto.getReplyText())
+				.replyWriter(getReplyId())
+				.replyPw(dto.getReplyPw())
+				.build();
+	}
+
+}
